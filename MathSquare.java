cdaf6100bb1de30892d14256b2d482fc365e08dd
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-07 20:11:25 +0400

    Update MathSquare.java

--- MathSquare.java
+++ MathSquare.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-
 public class MathSquare {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Input a positive integer: ");
-        int a = s.nextInt();
-        
-        if (a < 0) {
-            System.out.println("Enter a positive integer");
-            return;
-        }
-        
-        double square = Math.sqrt(a);
-        System.out.println(square);
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Input a positive integer: ");
+    int a = s.nextInt();
+
+    if (a < 0) {
+      System.out.println("Enter a positive integer");
+      return;
     }
+
+    double square = Math.sqrt(a);
+    System.out.println(square);
+  }
 }
